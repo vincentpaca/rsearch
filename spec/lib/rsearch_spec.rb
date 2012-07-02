61dfc0833a7c7debae66f837a0e1d255fb660fd7
Vincent Paca <vincent.paca@gmail.com>
2012-07-02 18:21:47 +0800

    Updated the spec

--- spec/lib/rsearch_spec.rb
+++ spec/lib/rsearch_spec.rb
@@ -4,7 +4,11 @@ describe RSearch do
   it "should return data when searching Google" do
     google = RSearch::Google.new("AIzaSyDZB5i7lI5t93u8sLM_-yNdz5OSrFqKUsU", "014249668475727183407:rf780m7ml6g")
     search = google.search("foo bar")
-    search.results.should_not be_nil
+    search.kind.should_not be_nil
+    search.url.should_not be_nil
+    search.queries.should_not be_nil
+    search.context.should_not be_nil
+    search.items.should_not be_nil
   end
  
 end
