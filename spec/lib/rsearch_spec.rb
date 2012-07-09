4cbf27616d86ad5c3c6dff514f039598bdbf1a74
Vincent Paca <vincent.paca@gmail.com>
2012-07-10 00:11:34 +0800

    Added tests

--- spec/lib/rsearch_spec.rb
+++ spec/lib/rsearch_spec.rb
@@ -3,13 +3,36 @@ require 'spec_helper'
 describe RSearch do
   it "should return data when searching Google" do
     google = RSearch::Google.new("AIzaSyDZB5i7lI5t93u8sLM_-yNdz5OSrFqKUsU", "014249668475727183407:rf780m7ml6g")
-    search = google.search("foo bar")
-    search.kind.should_not be_nil
+    search = google.search("ruby on rails")
     search.url.should_not be_nil
     search.queries.should_not be_nil
     search.context.should_not be_nil
     search.items.should_not be_nil
+
     search.url.type.should_not be_nil
+    search.url.template.should_not be_nil
+    search.queries.nextPage.should_not be_nil
+    search.queries.request.should_not be_nil
+    search.context.title.should_not be_nil
+    search.items.count.should == 10
+    search.items[0].kind.should_not be_nil
+    search.items[0].title.should_not be_nil
+    search.items[0].htmlTitle.should_not be_nil
+    search.items[0].link.should_not be_nil
+    search.items[0].displayLink.should_not be_nil
+    search.items[0].snippet.should_not be_nil
+    search.items[0].htmlSnippet.should_not be_nil
+  end
+
+  it "should return the correct number of results when results param is specified" do
+    google = RSearch::Google.new("AIzaSyDZB5i7lI5t93u8sLM_-yNdz5OSrFqKUsU", "014249668475727183407:rf780m7ml6g")
+    search = google.search("ruby on rails", { "start" => "1", "num" => "10" })
+    search.url.should_not be_nil
+    search.queries.should_not be_nil
+    search.context.should_not be_nil
+    search.items.should_not be_nil
+
+    search.items.count.should == 10
   end
  
 end
